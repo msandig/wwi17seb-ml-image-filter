bafd60cf0e9977e5bac63f1e47b10a78010387b3
msandig <MartinF95@web.de>
2020-07-04 18:37:26 +0200

    change stuff

--- plot.py
+++ plot.py
@@ -1,25 +1,29 @@
 
 import matplotlib.pyplot as plt
 
-def plot_keras_history(history, epochs):
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
+def plot_keras_history(history, epochs, metric):
+    acc = history.history[metric]
+    val_acc = history.history['val_' + metric]
 
-    loss=history.history['loss']
-    val_loss=history.history['val_loss']
+
 
     epochs_range = range(epochs)
 
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.plot(epochs_range, acc, label='Training ' + metric)
+    plt.plot(epochs_range, val_acc, label='Validation ' + metric)
     plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
+    plt.title('Training and Validation ' + metrics)
+    
+    if 'acc' in metric:
+        loss=history.history['loss']
+        val_loss=history.history['val_loss']
 
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        
     plt.show()
